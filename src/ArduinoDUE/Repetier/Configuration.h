408f2e6808c1a013464812458ea59dedde4b78bd
Dr. Bogger <drbogger@gmail.com>
2014-09-23 08:37:04 -0400

    Fixed Blank LCD / LCD Corruption Issue (Untested)

--- src/ArduinoDUE/Repetier/Configuration.h
+++ src/ArduinoDUE/Repetier/Configuration.h
@@ -330,7 +330,7 @@ WARNING: Servos can draw a considerable amount of current. Make sure your system
 #define FEATURE_BEEPER 1
 #define CASE_LIGHTS_PIN 85
 #define CASE_LIGHT_DEFAULT_ON 1
-#define UI_START_SCREEN_DELAY 2000
+#define UI_START_SCREEN_DELAY 4000
 /**
 Beeper sound definitions for short beeps during key actions
 and longer beeps for important actions.
